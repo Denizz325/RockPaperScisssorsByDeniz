f3cee8d3d5190cbdd5a7c40fc9a9d91960c7d037
Denizz325 <09aq.abv@gmail.com>
2023-09-24 12:47:53 +0300

    updated

--- Rock_Paper_Scissors.py
+++ Rock_Paper_Scissors.py
@@ -4,43 +4,41 @@ rock = 'Rock'
 paper = 'Paper'
 scissors = 'Scissors'
 
-player_move = input("Choose [r]ock, [p]aper or [s]cissors:")
+while True:
+    player_move = input("Choose [r]ock, [p]aper or [s]cissors:")
 
-if player_move == "r":
-    player_move = rock
+    if player_move == "r":
+        player_move = rock
+    elif player_move == "p":
+        player_move = paper
+    elif player_move == "s":
+        player_move = scissors
+    else:
+        print("Invalid Input. Try again...")
+        continue  # При невалиден вход продължаваме цикъла от начало
 
-elif player_move == "p":
-    player_move = paper
+    computer_random_move = random.randint(1, 3)
 
-elif player_move == "s":
-    player_move = scissors
+    computer_move = ''
 
-else:
-    raise SystemExit("Invalid Input. Try again...")
+    if computer_random_move == 1:
+        computer_move = rock
+    elif computer_random_move == 2:
+        computer_move = paper
+    elif computer_random_move == 3:
+        computer_move = scissors
 
-computer_random_move = random.randint(1, 3)
+    print(f"The computer chose {computer_move}.")
 
-computer_move = ''
+    if (player_move == rock and computer_move == scissors) or \
+            (player_move == paper and computer_move == rock) or \
+            (player_move == scissors and computer_move == paper):
+        print("You win")
+    elif player_move == computer_move:
+        print("Draw")
+    else:
+        print("You lose")
 
-if computer_random_move == 1:
-    computer_move = rock
-
-elif computer_random_move == 2:
-    computer_move = paper
-
-elif computer_random_move == 3:
-    computer_move = scissors
-
-print(f"The computer chose {computer_move}.")
-
-if (player_move == rock and computer_move == scissors) or \
-        (player_move == paper and computer_move == rock) or \
-        (player_move == scissors and computer_move == paper):
-    print("You win")
-
-if player_move == computer_move:
-    print("Draw")
-
-else:
-    print("You loose")
-
+    play_again = input("Do you want to continue playing? (yes/no): ")
+    if play_again.lower() != "yes":
+        break  # Ако потребителят не иска да продължи, излизаме от цикъла
